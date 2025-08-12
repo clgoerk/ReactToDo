c171feaa1f55ef55e2ae29fe0db0d9d25df7b706
clgoerk <Christopherlee.Goerk@triosstudent.com>
2025-08-12 09:15:34 -0400

    added checkbox toggle

--- todo/src/TodoRow.js
+++ todo/src/TodoRow.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-function TodoRow({item}) {
+function TodoRow({ item, toggle }) {
+  const onToggle = () => {
+    toggle(item);
+  };
 
   return (
     <tr>
@@ -11,6 +14,7 @@ function TodoRow({item}) {
         <input
           type="checkbox"
           checked={item.done}
+          onChange={onToggle}
         />
       </td>
     </tr>
